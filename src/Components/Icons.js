0f35a35a307c84245613328c4abe390e7b684ce7
jmmontanari <joshua.montanari@gmail.com>
2021-01-05 14:49:17 -0500

    added codepen icon

--- src/Components/Icons.js
+++ src/Components/Icons.js
@@ -4,11 +4,9 @@ import Icon from './Icon'
 const Icons = () => {
     return (
         <>
-            <div>
-                This is the section for the icons
-            </div>
             <Icon url='https://github.com/joshua-montanari' iconName={'Github'} iconUrl={'https://user-images.githubusercontent.com/50600343/103567240-71b76180-4e91-11eb-885e-e1a38d2738a4.png'} />
             <Icon url='https://www.linkedin.com/in/joshua-montanari/' iconName={'LinkedIn'} iconUrl={'https://user-images.githubusercontent.com/50600343/103567749-5e58c600-4e92-11eb-947a-28941847f63a.png'} />
+            <Icon url='https://codepen.io/joshua-montanari' iconName={'Codepen'} iconUrl={'https://user-images.githubusercontent.com/50600343/103691455-320d7a00-4f64-11eb-8125-6bfd763add56.png'} />
         </>
         
     )
